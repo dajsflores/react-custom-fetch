285e95e325176050329078432518de689779a3e1
davidDSuiza <davidflores@dsuiza.com.ar>
2022-08-02 14:37:53 -0300

    custom fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import useFetch from './hooks/CustomFetch/useFetch';
 
 function App() {
+  const { data, loading, error } = useFetch('https://rickandmortyapi.com/api/character');
+  console.log('data: ', data);
+  console.log('loading: ', loading);
+  console.log('error: ', error);
+
+  if (loading) return <h1>LOADING...</h1>
+  if (error) console.log(error);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Custom Hook</h1>
+      { data?.map( (character) => <p key={character.id}>{character.name}</p> ) }
     </div>
   );
 }
